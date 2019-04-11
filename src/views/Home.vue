7d4e21f09717a4c121d22172854127adf7b99c7f
Daniel Hovorka <dhovorka10@gmail.com>
2019-04-10 19:32:09 -0500

    initial commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,32 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="../assets/logo.png" /> -->
+    <!-- <HelloWorld msg="Welcome to THE JUNGLE" /> -->
+    <h1>All Tournaments</h1>
+    <div v-for="tournament in tournaments">
+      <h2>{{ tournament.name }}</h2>
+      <p>Location: {{ tournament.location }}</p>
+      <p>Information: {{ tournament.description }}</p>
+    </div>
   </div>
 </template>
 
 <script>
+import axios from "axios";
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
+  data: function() {
+    return {
+      tournaments: []
+    };
+  },
+  created: function() {
+    axios.get("/api/tournaments").then(response => {
+      this.tournaments = response.data;
+    });
+  },
+  methods: {}
+};
 </script>
