b626dcfc1ef2d653c0e13eb8f389b952a6bdae10
Daniel Hovorka <dhovorka10@gmail.com>
2019-04-10 19:46:29 -0500

    added tournaments index file & route

---
+++ src/views/TournamentsIndex.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="index">
+    <h1>All Tournaments</h1>
+    <div v-for="tournament in tournaments">
+      <h2>{{ tournament.name }}</h2>
+      <p>Location: {{ tournament.location }}</p>
+      <p>Information: {{ tournament.description }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data: function() {
+    return {
+      tournaments: []
+    };
+  },
+  created: function() {
+    axios.get("/api/tournaments").then(response => {
+      this.tournaments = response.data;
+    });
+  },
+  methods: {}
+};
+</script>
